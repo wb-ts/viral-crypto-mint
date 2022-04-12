c4815f55628866fc9e198986ef7d7b9e7a5630cd
SenDev001 <sergeilipovsky@gmail.com>
2022-04-12 23:51:57 +0800

    update more wallets

--- src/providerOptions.js
+++ src/providerOptions.js
@@ -17,13 +17,7 @@ export const providerOptions = {
     options: {
       infuraId: process.env.REACT_APP_INFURA_KEY // required
     },
-    qrcode: true,
-    qrcodeModalOptions: {
-        mobileLinks: [
-          "metamask",
-          "trust",
-        ]
-    }
+    qrcode: true
     
   }
 };
